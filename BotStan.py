12d771e14437b8d066ae131dca23ee68cd2fe5c5
Kayo-HS <kayohsantos0@gmail.com>
2025-05-13 18:10:17 -0300

    Tentando colocar jogos no botStan

--- BotStan.py
+++ BotStan.py
@@ -30,7 +30,32 @@ async def falar(ctx:commands.Context, *,frase):
 
 botStan.add_command(StanCaculadora.calcular)
 
+@botStan.command()
+async def jogos(ctx:commands.Context):
+    embed = discord.Embed(title="Vamos no divertir", description= "Escolha uma opção:", color=discord.Color.blue())
+    embed.add_field(name= "Opção 1:", value= "Pedra, papel e tesoura", inline=False)
+    embed.add_field(name= "Opção 2:", value= "Pedra, papel e tesoura", inline=False)
+    embed.add_field(name= "Opção 3:", value= "Pedra, papel e tesoura", inline=False)
+    msg = await ctx.send(embed=embed)
 
+    await msg.add_reaction("1")
+    await msg.add_reaction("2")
+    await msg.add_reaction("3")
+
+    def check (reaction, user):
+        return user == ctx.author and str(reaction.str) in ["1", "2", "3"]
+    
+    try:
+        reaction, user = await botStan.wait_for("reaction_add", timeout=60.0, check=check)
+
+        if str(reaction.str) == "1":
+            await ctx.send('Você escolheu a Opção 1!')
+        elif str(reaction.str) == "2":
+            await ctx.send('Você escolheu a Opção 2!')
+        elif str(reaction.str) == "3":
+            await ctx.send('Você escolheu a Opção 3!')
+    except StopAsyncIteration.TimeoutError:
+        await ctx.send('Tempo esgotado!')
 
 
 
