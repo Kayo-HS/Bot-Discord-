53fd478ca9ceeecd447d38631a60c3f467a4f9d5
Kayo-HS <kayohsantos0@gmail.com>
2025-05-15 23:54:10 -0300

    Tentando adicionar emoji aos jogos

--- JogosStan.py
+++ JogosStan.py
@@ -9,21 +9,21 @@ async def jogos(ctx:commands.Context):
     embed.add_field(name= "Opção 3:", value= "Pedra, papel e tesoura", inline=False)
     msg = await ctx.send(embed=embed)
 
-    await msg.add_reaction("1")
-    await msg.add_reaction("2")
-    await msg.add_reaction("3")
+    await msg.add_reaction("1️⃣")
+    await msg.add_reaction("2️⃣")
+    await msg.add_reaction("3️⃣")
 
     def check (reaction, user):
-        return user == ctx.author and str(reaction.str) in ["1", "2", "3"]
+        return user == ctx.author and str(reaction.emoji) in ["1️⃣", "2️⃣", "3️⃣"]
     
     try:
-        reaction, user = await BotStan.wait_for("reaction_add", timeout=60.0, check=check)
+        reaction, user = await commands.wait_for("reaction_add", timeout=60.0, check=check)
 
-        if str(reaction.str) == "1":
+        if str(reaction.emoji) == "1️⃣":
             await ctx.send('Você escolheu a Opção 1!')
-        elif str(reaction.str) == "2":
+        elif str(reaction.emoji) == "2️⃣":
               await ctx.send('Você escolheu a Opção 2!')
-        elif str(reaction.str) == "3":
+        elif str(reaction.emoji) == "3️⃣":
            await ctx.send('Você escolheu a Opção 3!')
     except StopAsyncIteration.TimeoutError:
         await ctx.send('Tempo esgotado!')
